2b842bfd449f0ccdc948f28b7f195e936984089f
yixin-cfd <yixincfd@gmail.com>
2024-12-02 10:24:08 +0800

    mpu6050 utf-8

--- 10.MPU6050/components/mpu6050/eMPL/inv_mpu.h
+++ 10.MPU6050/components/mpu6050/eMPL/inv_mpu.h
@@ -24,7 +24,7 @@
 #include "../mpu6050.h"
 #define u8 uint8_t
 
-//¶¨ÒåÊä³öËÙ¶È
+//å®šä¹‰è¾“å‡ºé€Ÿåº¦
 #define DEFAULT_MPU_HZ  (100)		//100Hz
 
 #define INV_X_GYRO      (0x40)
@@ -34,7 +34,7 @@
 #define INV_XYZ_ACCEL   (0x08)
 #define INV_XYZ_COMPASS (0x01)
 
-//ÒÆÖ²¹Ù·½MSP430 DMPÇı¶¯¹ıÀ´
+//ç§»æ¤å®˜æ–¹MSP430 DMPé©±åŠ¨è¿‡æ¥
 struct int_param_s {
 //#if defined EMPL_TARGET_MSP430 || defined MOTION_DRIVER_TARGET_MSP430
     void (*cb)(void);
@@ -129,7 +129,7 @@ int mpu_reg_dump(void);
 int mpu_read_reg(unsigned char reg, unsigned char *data);
 int mpu_run_self_test(long *gyro, long *accel);
 int mpu_register_tap_cb(void (*func)(unsigned char, unsigned char));
-//×ÔĞĞÌí¼ÓµÄÒ»Ğ©º¯Êı
+//è‡ªè¡Œæ·»åŠ çš„ä¸€äº›å‡½æ•°
 void delay_ms(uint32_t t);
 void mget_ms(unsigned long *time);
 unsigned short inv_row_2_scale(const signed char *row);
